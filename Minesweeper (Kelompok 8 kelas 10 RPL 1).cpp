3a559065c66b502116f9bc53ce929c8526450cad
StallPlayz <stallplayz123@gmail.com>
2025-01-09 10:30:15 +0700

    Add files via upload

---
+++ Minesweeper (Kelompok 8 kelas 10 RPL 1).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main () {
+	srand(time(0));
+	int tebak, minim = 1, max = 100;
+	int bom = rand() %100+1;
+	cout << "Permainan Awas Bom!\n" << "\nMasukkan angka dan selalu hindari bom, jika kena... \n\n DUAR!!\n";
+
+	while (true) {
+		cout << "\nMasukkan tebakan mulai dari " << minim << " hingga " << max << endl;
+		cin >> tebak;
+		if (tebak<bom) {
+			if(tebak<minim) {
+				cout << "Range menebak angka dari " << minim << " hingga " << max << "\n\n";
+			} else {
+				minim = tebak + 1;
+				cout << "\nTebakan bukan bom! Kamu aman!\n" << "Range dimulai dari " << minim << " Hingga " << max << endl;
+			}
+		} else if (tebak>bom) {
+			if (tebak>max) {
+				cout << "Range menebak angka dari " << minim << " hingga " << max << "\n\n";
+			} else {
+				max = tebak - 1;
+				cout << "\nTebakan bukan bom! Kamu aman!\n" << "Range dimulai dari " << minim << " Hingga " << max << endl;
+			}
+		} else {
+			cout << "\nDuar!!\n" << "Kamu kena bom! Yahaha gagal";
+			break;
+		}
+		if (minim==bom) {
+			if (max==bom) {
+				cout << "Selamat!! Kamu Menang!\n" << "Bom ada di " << bom;
+				break;
+			}
+		}
+	}
+}
